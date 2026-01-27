1f512ea2afdf076b4f7fffe03b2086fb091ff5e7
yukonisen#QwQ <whenten99@gmail.com>
2026-01-27 10:07:08 +0800

    更新插件模板

--- plugin/src/main/kotlin/com/example/plugin/ExampleWebDataSource.kt
+++ plugin/src/main/kotlin/com/example/plugin/ExampleWebDataSource.kt
@@ -5,9 +5,9 @@ import io.nightfish.lightnovelreader.api.book.BookVolumes
 import io.nightfish.lightnovelreader.api.book.ChapterContent
 import io.nightfish.lightnovelreader.api.web.WebBookDataSource
 import io.nightfish.lightnovelreader.api.web.WebDataSource
-import io.nightfish.lightnovelreader.api.web.explore.ExploreExpandedPageDataSource
-import io.nightfish.lightnovelreader.api.web.explore.ExplorePageDataSource
-import kotlinx.coroutines.flow.Flow
+import io.nightfish.lightnovelreader.api.web.explore.ExplorePageProvider
+import io.nightfish.lightnovelreader.api.web.search.SearchProvider
+import kotlinx.coroutines.flow.StateFlow
 
 @WebDataSource(
     name = "ExampleWebDataSource",
@@ -18,19 +18,12 @@ class ExampleWebDataSource: WebBookDataSource {
         get() = "example".hashCode()
     override val offLine: Boolean
         get() = TODO("Not yet implemented")
-    override val isOffLineFlow: Flow<Boolean>
+    override val isOffLineFlow: StateFlow<Boolean>
         get() = TODO("Not yet implemented")
-    override val explorePageIdList: List<String>
+
+    override val searchProvider: SearchProvider
         get() = TODO("Not yet implemented")
-    override val explorePageDataSourceMap: Map<String, ExplorePageDataSource>
-        get() = TODO("Not yet implemented")
-    override val exploreExpandedPageDataSourceMap: Map<String, ExploreExpandedPageDataSource>
-        get() = TODO("Not yet implemented")
-    override val searchTypeMap: Map<String, String>
-        get() = TODO("Not yet implemented")
-    override val searchTipMap: Map<String, String>
-        get() = TODO("Not yet implemented")
-    override val searchTypeIdList: List<String>
+    override val explorePageProvider: ExplorePageProvider
         get() = TODO("Not yet implemented")
 
     override suspend fun isOffLine(): Boolean {
@@ -49,17 +42,6 @@ class ExampleWebDataSource: WebBookDataSource {
         chapterId: String,
         bookId: String
     ): ChapterContent {
-        TODO("Not yet implemented")
-    }
-
-    override fun search(
-        searchType: String,
-        keyword: String
-    ): Flow<List<BookInformation>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun stopAllSearch() {
         TODO("Not yet implemented")
     }
 
